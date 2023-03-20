26d504799be99de6f9eaf352fc89aa4e9e6b3cfa
unknown <parmarsinghbhanu@gmail.com>
2023-03-20 21:27:09 +0530

    some change

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 const path=require("path");
 
+
 app.use(express.static('client/build'));
  app.get('*', (req, res) => {
     res.sendFile(path.resolve('client','build','index.html'));
