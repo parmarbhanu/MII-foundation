98057472017d24e54c2b8243f11f2e8587174d7b
Girish Ladha <grirajladha32@gmail.com>
2023-03-27 18:49:15 +0530

    first re edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 // import { Route,Routes } from 'react-router-dom';
-import Footer from './Universal/Footer';
+ import Footer from './Universal/Footer';
 // import Header from './Universal/Header';
 // import Herosection from './Pages/Home/Herosection/Herosection';
 // import LandingPage from './Pages/Home/LandingPage';
 // import About from "./Pages/AboutUs/About"
-import Ourpartner from "./Pages/AboutUs/Ourpartner"
-import Feature from './Feature/Feature';
+// import Ourpartner from "./Pages/AboutUs/Ourpartner"
+// import Feature from './Feature/Feature';
 function App() {
   return (
     <div className="App">
@@ -14,11 +14,11 @@ function App() {
          {/* <Routes> */}
          {/* <Herosection/>
          <LandingPage/> */}
-         <Ourpartner/>
+         {/* <Ourpartner/> */}
          {/* <About/> */}
          {/* <Route  path='/about' element={<About/> }/> */}
         {/* </Routes> */}
-        <Feature/>
+        {/* <Feature/> */}
       <Footer/>
     </div>
   );
