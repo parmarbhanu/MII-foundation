f0d1abd9207136490ff05f0176875ba258d0fa54
unknown <parmarsinghbhanu@gmail.com>
2023-03-27 21:36:10 +0530

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,26 @@ import './App.css';
 // import { Route,Routes } from 'react-router-dom';
 import Footer from './Universal/Footer';
 // import Header from './Universal/Header';
-// import Herosection from './Pages/Home/Herosection/Herosection';
-// import LandingPage from './Pages/Home/LandingPage';
+import Herosection from './Pages/Home/Herosection/Herosection';
+import LandingPage from './Pages/Home/LandingPage';
 // import About from "./Pages/AboutUs/About"
 import Ourpartner from "./Pages/AboutUs/Ourpartner"
-import Feature from './Feature/Feature';
+import About from './Pages/AboutUs/About';
+// import Feature from './Pages/Feature/Feature';
+
 function App() {
   return (
     <div className="App">
       {/* <Header/> */}
          {/* <Routes> */}
-         {/* <Herosection/>
-         <LandingPage/> */}
+        
+         <LandingPage/>
          <Ourpartner/>
+         <Herosection/>
          {/* <About/> */}
-         {/* <Route  path='/about' element={<About/> }/> */}
+        <About/>
         {/* </Routes> */}
-        <Feature/>
+        {/* <Feature/> */}
       <Footer/>
     </div>
   );
--- client/src/Pages/AboutUs/About.jsx
+++ client/src/Pages/AboutUs/About.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
-import OurTeam from "./OurTeam"
-import OurAdvisor from "./OurAdvisor"
 
 export default function About() {
   return (
-    <div>
-      <OurTeam/>
-      <OurAdvisor/>
-
-    </div>
+    <div>About</div>
   )
 }
